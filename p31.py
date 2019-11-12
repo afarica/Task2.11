9b73cee46748fd8535e85dd6a034dff00248ba5c
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 07:27:30 +0600

    Update p31.py

--- p31.py
+++ p31.py
@@ -1,14 +1,5 @@
 # Given a string consisting of words separated by spaces. Determine how many
 # words it has. To solve the problem, use the method count.
-a=list(input("Enter your string:"))
-b=a[0]
-d=a[-1]
-c=a.count(" ")
-if b==" " and d==" ":
-	print("In this string " + str(c-1) +" words")
-elif d==" ":
-	print("In this string " + str(c) +" words")
-elif b==" ":
-	print("In this string " +str(c) +"words")
-else:
-	print("In this string " + str(c+1)+"words")	
+string=list(input("Enter your string:")).strip()
+a=string.count(" ")
+print(a+1,' word(s)')
